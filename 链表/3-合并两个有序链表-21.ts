bd679e13b82163098217d65a021cefd2be26a801
Mr.yao <Mr.yao.email@qq.com>
2023-08-02 21:46:29 +0800

    feat: update

---
+++ 链表/3-合并两个有序链表-21.ts
@@ -0,0 +1,22 @@
+// https://leetcode.cn/problems/merge-two-sorted-lists/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+interface ListNode {
+    value: any
+    next: any
+}
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    return null
+};
